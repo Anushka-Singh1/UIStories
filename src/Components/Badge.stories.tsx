bec2bec4a1b5b3f04e37fd9c0bfa35b8372916e5
Anushka-Singh1 <anushka.sng2002@gmail.com>
2025-04-11 00:46:42 +0530

    Updated code section

--- src/Components/Badge.stories.tsx
+++ src/Components/Badge.stories.tsx
@@ -2,22 +2,38 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Badge } from "./Badge";
 
 const meta: Meta<typeof Badge> = {
-  component: Badge,
   title: "Components/Badge",
+  component: Badge,
   tags: ["autodocs"],
+  parameters: {
+    docs: {
+      source: {
+        code: `
+type Badgeprop = {
+  label: string;
+  color: string;
+}
+
+export const Badge = ({ label, color }: Badgeprop) => {
+  return (
+    <span className={\`bg-\${color}-500 text-white px-2 py-1 rounded\`}>
+      {label}
+    </span>
+  );
+};`,
+        language: "tsx",
+      },
+    },
+  },
 };
+
 export default meta;
+
 type Story = StoryObj<typeof Badge>;
 
 export const Primary: Story = {
   args: {
     label: "Badge",
     color: "blue",
-  },
-};
-export const Secondary: Story = {
-  args: {
-    label: "Badge",
-    color: "green",
   },
 };
