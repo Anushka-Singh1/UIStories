8932bffb9d634c47dcb809756ef2b0a2bbe4a43e
Anushka-Singh1 <anushka.sng2002@gmail.com>
2025-04-27 14:48:25 +0530

    Added Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-
-
   return (
     <>
       <div className="text-center mt-10 text-2xl text-blue-500 font-bold">
-        Hello Tailwind + Vite! 
+        Hello Tailwind + Vite!
       </div>
+      <Analytics />
     </>
   );
 }
 
-export default App
+export default App;
